d42ec2fd5b5c1ef736ed07ea2f39cfcc9a420487
TheMonHub <mono15.parnthakote@gmail.com>
2026-03-18 22:13:31 +0700

    adjust intro

--- src/scenes/intro.lua
+++ src/scenes/intro.lua
@@ -2,9 +2,12 @@ local game = {}
 
 local gameLog = require("log.main")
 local ease = require("render.ease")
+local text
 local start = 0
+local wait = 0
 
 function game:load(args)
+    text = require("text.main")
 end
 
 function game:draw()
@@ -12,10 +15,16 @@ function game:draw()
     love.graphics.rectangle("fill", -400, -300, 800, 600)
     love.graphics.setColor(1,1,1,start)
     love.graphics.rectangle("fill", -400, -300, 800, 600)
+    love.graphics.setColor(0,0,0,start)
+    text.print("COMPANION", 0, 0, 7.5, 7.5)
 end
 
 function game:update(dt)
     if start >= 0.95 then
+        if wait < 0.25 then
+            wait = wait + dt
+            return
+        end
         self.setScene("main-game")
         return
     end
